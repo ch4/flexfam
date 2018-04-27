9bfa5b752ecc07cd5038351bf58e7c7ca6091de9
Harvey Chan <ch4ch4@gmail.com>
2018-04-27 16:47:51 -0700

    add jqm

--- server.js
+++ server.js
@@ -61,11 +61,13 @@ app.use(require('express-session')({ secret: 'keyboard cat', resave: false, save
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(express.static('public'))
+
 // Define routes.
-app.get('/',
-  function(req, res) {
-    res.render('home', { user: req.user });
-  });
+// app.get('/',
+//   function(req, res) {
+//     res.render('home', { user: req.user });
+//   });
 
 app.get('/login',
   function(req, res){
