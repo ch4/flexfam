df79efc24501ca9cea505f5b85b4473159ddc904
Harvey Chan <ch4ch4@gmail.com>
2018-04-30 14:54:04 -0700

    tweak bodyparser

--- server.js
+++ server.js
@@ -98,7 +98,8 @@ app.set('view engine', 'ejs');
 // logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
-app.use(require('body-parser').json({ extended: true }));
+app.use(require('body-parser').urlencoded({ extended: false }));
+app.use(require('body-parser').json({ extended: false }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 // Initialize Passport and restore authentication state, if any, from the
