709fdce275d6fb27bc0fd5506269d2e7913cc580
Harvey Chan <ch4ch4@gmail.com>
2018-04-30 14:51:47 -0700

    print body

--- server.js
+++ server.js
@@ -198,7 +198,7 @@ app.post('/sms',
     const twiml = new MessagingResponse();
 
     twiml.message('The Robots are coming! Head for the hills!');
-    console.log('twilio request', req);
+    console.log('twilio request', req.body);
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
